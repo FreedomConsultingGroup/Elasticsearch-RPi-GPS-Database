a08446f4d3f2a0c44fea7cf3cdae82b93625d8f0
cgood404 <chrisgood404@gmail.com>
2018-03-09 14:32:02 -0500

    Added usernames and passwords to mosquitto, read from separate file

--- mqttelasticsearch.py
+++ mqttelasticsearch.py
@@ -7,9 +7,13 @@ import time, memory
 
 def main():
     keys = open("/home/ubuntu/chrisStuff/keys.txt", 'r')
+    usrfile = open("/home/ubuntu/chrisStuff/usrfile.pswd")
     aws_key = keys.readline().replace('\n', '')
     aws_secret = keys.readline().replace('\n', '')
     google_api_key = keys.readline().replace('\n', '')
+    usrnm = usrfile.readline().replace('\n', '')
+    passwd = usrfile.readline().replace('\n', '')
+    usrfile.close()
     keys.close()
 
     region = "us-east-1"
@@ -32,6 +36,7 @@ def main():
             print(str(payload))
 
         client = mqtt.Client('ec2instance', clean_session=False, userdata='ec2instance')
+        client.username_pw_set(usrnm, passwd)
         client.on_connect = on_connect
         client.on_message = on_message
         client.connect('127.0.0.1', 1883, 60)
