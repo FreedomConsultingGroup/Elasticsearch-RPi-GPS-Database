13330bf5a14b2cb37428ce3bb65aaf31cd6ee6c2
cgood404 <chrisgood404@gmail.com>
2018-06-07 10:05:41 -0400

    python won't run the code for some reason

--- Server_Files/mqttelasticsearch.py
+++ Server_Files/mqttelasticsearch.py
@@ -47,7 +47,7 @@ def main():
         client.on_message = on_message
         client.connect('127.0.0.1', 1883, 60)
 
-        client.loop_start()
+        client.loop_forever()
     finally:
         mem.stop_threads()
 
